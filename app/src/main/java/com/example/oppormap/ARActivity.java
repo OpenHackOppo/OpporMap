c47036e8c116775b2c9f8778e25c75b7ed7ab439
Nuculais <none>
2019-11-03 02:42:57 +0100

    started AR

--- app/src/main/java/com/example/oppormap/ARActivity.java
+++ app/src/main/java/com/example/oppormap/ARActivity.java
@@ -1,6 +1,9 @@
 package com.example.oppormap;
+package com.google.ar.core.examples.java.common.helpers;
 
 import androidx.appcompat.app.AppCompatActivity;
+import com.google.ar.core.examples.java.augmentedimage.rendering.AugmentedImageRenderer;
+import com.google.ar.core.examples.java.common.helpers.CameraPermissionHelper;
 
 import android.os.Bundle;
 
@@ -11,4 +14,21 @@ public class ARActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ar);
     }
-}
+
+//    @Override
+//    protected void onResume() {
+//        super.onResume();
+//
+//        // ARCore requires camera permission to operate.
+//        if (!CameraPermissionHelper.hasCameraPermission(this)) {
+//            CameraPermissionHelper.requestCameraPermission(this);
+//            return;
+//        }
+//
+
+
+
+
+
+
+    }
